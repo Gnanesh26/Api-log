50423612eb1196878140882fd90abaa95dd086e7
gnanesh26 <gnaneswarrao.yeddu@imaginnovate.com>
2023-07-17 12:13:47 +0530

    commit

--- api-logdata/src/main/java/testing/apilogdata/Controller/LogController.java
+++ api-logdata/src/main/java/testing/apilogdata/Controller/LogController.java
@@ -20,28 +20,51 @@ import java.util.List;
 public class LogController {
     @Autowired
     LogService logService;
+    private Bucket bucket;
 
 
-    private final Bucket bucket;
+    @GetMapping("/token-generate")
+    public ResponseEntity<String> generateToken(){
+        Refill refill = Refill.of(5,Duration.ofMinutes(1));
+      bucket=Bucket4j.builder().addLimit(Bandwidth.classic(5,refill)).build();
+        return new ResponseEntity<String>("Generated Successfully |||"+bucket.toString(),HttpStatus.OK);
+    }
 
+
+
+    @GetMapping("/demo")
+    public ResponseEntity<String> demo(){
+        if(bucket.tryConsume(1)) {
+System.out.println("---------------API WORKING SUCCESSFULLY---------------");
+
+            return new ResponseEntity<String>("Success", HttpStatus.OK);
+        }
+        System.out.println("---------------NUMBER OF HiTS EXCEEDED");
+        return new ResponseEntity<String>("TOO MANY HITS !!! PLEASE TRY LATER",HttpStatus.TOO_MANY_REQUESTS);
+
+    }
+
+//    private final Bucket bucket;
+//
     public LogController() {
         Bandwidth limit = Bandwidth.classic(5, Refill.greedy(5, Duration.ofMinutes(2)));
         this.bucket = Bucket4j.builder()
                 .addLimit(limit)
                 .build();
     }
-
-
+//
+//
     @GetMapping("/log")
     public ResponseEntity<List<LogDTO>> getAllLogs() {
         if (bucket.tryConsume(1)) {
             List<LogDTO> logs = logService.getAllLogs();
+
             return ResponseEntity.ok(logs);
         } else {
             return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
         }
     }
-}
+//}
 
 //@RestController
 //
@@ -56,3 +79,4 @@ public class LogController {
 ////
 //}
 //}
+}
---
+++ api-logdata/src/main/java/testing/apilogdata/Service/PricingPlan.java
@@ -0,0 +1,40 @@
+//package testing.apilogdata.Service;
+//
+//import io.github.bucket4j.Bandwidth;
+//import io.github.bucket4j.Bucket;
+//import io.github.bucket4j.Bucket4j;
+//import io.github.bucket4j.Refill;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//
+//import java.time.Duration;
+//
+//@Service
+//public class PricingPlan {
+//    @Value("rate.limit.client.basic")
+//    private String basic;
+//
+//
+//    @Value("rate.limit.client.free")
+//    private String free;
+//
+//    public Bucket getPlanServiceBucket(String clientToken){
+//        return Bucket4j.builder().addLimit(getclientBnadwidth(clientToken)).build();
+//
+//    }
+//
+//    private Bandwidth getclientBnadwidth(String clientToken) {
+//
+//        if(clientToken.equals("basic123"))
+//            return Bandwidth.classic(5, Refill.of(5, Duration.ofMinutes(1)));
+//        else if (clientToken.equals("free123"))
+//            return Bandwidth.classic(10, Refill.of(10, Duration.ofMinutes(1)));
+//
+//            return  Bandwidth.classic(2,Refill.of(2,Duration.ofMinutes(1)));
+//
+//        }
+//
+//    }
+//
+//
+//
